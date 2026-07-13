91b9fd2a02f1605575141ed73be9551a60eec280
Ivan Gordillo <gordillosolis@icloud.com>
2026-07-13 04:01:42 -0600

    Feat: Mermaid diagrams support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mermaid from "astro-mermaid";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [
+        mermaid({
+            theme: "base",
+            autoTheme: true,
+            // mermaidConfig: {
+            //     themeVariables: {
+            //         primaryColor: "#111827",
+            //         primaryTextColor: "#f9fafb",
+            //         lineColor: "#6b7280",
+            //         fontFamily: "Inter, ui-sans-serif, system-ui",
+            //     },
+            // },
+        }),
+    ],
 });
